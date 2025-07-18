8be53f7015c79c74f537148a20e054489440043c
Jimbob6697 <info@inhouseyork.com>
2025-07-18 16:21:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,4 +11,7 @@ app.use('/api', analyzeRoute);
 app.use('/test', testRoute); // ← ADD THIS
 
 const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.send('API is running. Use /api or /test endpoints.');
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
