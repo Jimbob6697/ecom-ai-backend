eb69c45c503ba7ee5533970d14ca7273dc3eb9f0
Jimbob6697 <info@inhouseyork.com>
2025-07-18 16:19:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const app = express();
 
 require('dotenv').config();
 const analyzeRoute = require('./routes/analyze');
+const testRoute = require('./routes/test'); // ← ADD THIS
+
 app.use(express.json());
 app.use('/api', analyzeRoute);
+app.use('/test', testRoute); // ← ADD THIS
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
